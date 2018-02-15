c22c54b5111cbec7c619b41a2a97b27f7c040f8f
STsaruk <s0936771414@gmail.com>
2018-02-15 18:55:24 +0200

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://images.contentful.com/uexfe9h31g3m/6cXUnG9vTasyUoCQAiIUAc/94619f709' +
+      'dd48dc4dddcc03774bb7f55/Kebabish_Style_Nuggets_Recipe_Web__2_.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
