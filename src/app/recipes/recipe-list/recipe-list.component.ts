efd94b7cdff70c9a658d2ebe2edd25821d269861
STsaruk <s0936771414@gmail.com>
2018-02-16 20:21:28 +0200

    next lessons

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'A Test Recipe',
       'This is simply a test',
       'https://images.contentful.com/uexfe9h31g3m/6cXUnG9vTasyUoCQAiIUAc/94619f709' +
+      'dd48dc4dddcc03774bb7f55/Kebabish_Style_Nuggets_Recipe_Web__2_.jpg'),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://images.contentful.com/uexfe9h31g3m/6cXUnG9vTasyUoCQAiIUAc/94619f709' +
       'dd48dc4dddcc03774bb7f55/Kebabish_Style_Nuggets_Recipe_Web__2_.jpg')
   ];
 
